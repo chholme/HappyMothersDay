c1cbcfd35a15d735be9e1852d7515c89d3edf5a6
chholme <chholme@iu.edu>
2024-05-11 20:11:45 -0400

    getting headers to work

--- fixedHeaders.js
+++ fixedHeaders.js
@@ -7,12 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
     headers.forEach((header, index) => {
       const fixedHeader = header.parentElement;
 
-      if (scrollPosition >= fixedHeader.offsetTop) {
+      // Get the offsetTop of the next sibling element
+      const nextSiblingOffsetTop = fixedHeader.nextElementSibling.offsetTop;
+
+      if (scrollPosition >= nextSiblingOffsetTop - 430) {
+        // Make the fixedHeader fixed
         fixedHeader.style.position = "fixed";
         fixedHeader.style.top = `${index * header.offsetHeight}px`; // Adjust the top position based on index
         fixedHeader.style.left = "0";
         fixedHeader.style.width = "100%";
-
         fixedHeader.style.zIndex = "1000"; // Adjust z-index if needed
       } else {
         fixedHeader.style.position = "static";
